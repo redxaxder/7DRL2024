3e9097bba6720e32c7e4af299243b64dfb4b8c49
redxaxder <abyaly@gmail.com>
2024-03-02 14:20:56 -0600

    wip uncursed

--- data/encounter/EncounterHistory.gd
+++ data/encounter/EncounterHistory.gd
@@ -1,19 +1,19 @@
 extends Resource
 
 class_name EncounterHistory
-export var states: Array = [] # Array of EncounterState
-export var events: Array = [] # Array of EncounterEvent
+export var _states: Array = [] # Array of EncounterState
+export var _events: Array = [] # Array of EncounterEvent
 # events[i] is the event that causes a transition from
 # state[i] to state[i+1]
 
 func add_state(st: EncounterState):
-	states.append(states)
-	for i in events.size():
-		var e = events[i]
-		assert(e is EncounterEvent)
+	_states.append(st)
 
 func add_event(evt: EncounterEvent):
-	events.append(evt)
-	for i in events.size():
-		var e = events[i]
-		assert(e is EncounterEvent)
+	_events.append(evt)
+
+func get_states() -> Array:
+	return _states
+
+func get_events() -> Array:
+	return _events
--- playback/ViewHistory.gd
+++ playback/ViewHistory.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export var history: Resource = null
+#export var history: Resource = null
 export var cursor: int = 0
 
 func _ready():
@@ -10,7 +10,7 @@ func _ready():
 		some_event.delta = (randi() % 5) - 2
 		some_events.append(some_event)
 	var state0 = EncounterState.new()
-	history = roll_up(state0, some_events)
+#	history = roll_up(state0, some_events)
 # warning-ignore:return_value_discarded
 	get_node("%to_start").connect("button_down", self, "to_start")
 # warning-ignore:return_value_discarded
@@ -18,7 +18,7 @@ func _ready():
 
 	var progressbar = get_node("%progress_bar")
 	progressbar.min_value = 0
-	progressbar.max_value = history.states.size() - 1
+#	progressbar.max_value = history.states.size() - 1
 	progressbar.step = 1
 	progressbar.connect("scrolling", self, "progress_bar_scroll")
 
@@ -27,36 +27,36 @@ func _ready():
 	for _i in 10:
 		var _player_is_dead = driver.tick()
 	
-	for evt in driver.history.events:
+	for evt in driver.history.get_events():
 		assert(evt != null)
 		assert(evt is EncounterEvent)
 		var text = driver.event_text(evt)
 		print(text)
 
 
-func roll_up(state0: EncounterState, events: Array) -> EncounterHistory:
-	var h = EncounterHistory.new()
-	h.states = [state0]
-	h.events = events
-	h.states.resize(events.size() + 1)
-	for i in events.size():
-		var e = events[i]
-		var s = h.states[i]
-		var next_s = DataUtil.update(DataUtil.dup_state(s),e)
-		h.states[i+1] = next_s
-	return h
+#func roll_up(state0: EncounterState, events: Array) -> EncounterHistory:
+#	var h = EncounterHistory.new()
+#	h._states = [state0]
+#	h.events = events
+#	h.states.resize(events.size() + 1)
+#	for i in events.size():
+#		var e = events[i]
+#		var s = h.states[i]
+#		var next_s = DataUtil.update(DataUtil.dup_state(s),e)
+#		h.states[i+1] = next_s
+#	return h
 
 func to_start():
 	cursor = 0
 	_refresh()
 
 func to_end():
-	cursor = history.states.size() - 1
+#	cursor = history.states.size() - 1
 	_refresh()
 
 func next():
 # warning-ignore:narrowing_conversion
-	cursor = min(cursor+1, history.states.size() - 1)
+#	cursor = min(cursor+1, history.states.size() - 1)
 	_refresh()
 
 func prev():
@@ -89,14 +89,15 @@ func allocate_sprites(s: EncounterState):
 # hard refresh for viewing new encounters
 # ordinary refresh for viewing different state of same encounter
 func _refresh(hard_refresh: bool = false):
-	var current_state = history.states[cursor]
-	if hard_refresh:
-		allocate_sprites(current_state)
-	
-	var n = current_state.actors.size()
-	for i in n:
-		var actor = current_state.actors[n]
-		sprites[i].position = actor.location * 100
-
-	var progressbar = get_node("%progress_bar")
-	progressbar.value = cursor
+	pass
+#	var current_state = history.states[cursor]
+#	if hard_refresh:
+#		allocate_sprites(current_state)
+#
+#	var n = current_state.actors.size()
+#	for i in n:
+#		var actor = current_state.actors[n]
+#		sprites[i].position = actor.location * 100
+#
+#	var progressbar = get_node("%progress_bar")
+#	progressbar.value = cursor
