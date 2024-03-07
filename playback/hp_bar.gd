cf7af107e1e09168b96bebb3af806a99a184be9c
redxaxder <abyaly@gmail.com>
2024-03-07 12:15:10 -0600

    fix the hp bar fix

--- playback/hp_bar.gd
+++ playback/hp_bar.gd
@@ -12,9 +12,10 @@ const FOREGROUND: Color = Color(0,1,0)
 const BACKGROUND: Color = Color(0.4,0.4,0.4)
 
 func _draw():
-	var x = round(fill_percent * breakpoints) / float(breakpoints)
+	var x = round(fill_percent * breakpoints)
 	if fill_percent > 0:
 		x = max(x,1)
+	x = x / float(breakpoints)
 	var sz
 	var filled: Rect2 = Rect2(Vector2.ZERO, Vector2(x,1) * rect_size)
 	draw_rect(filled, FOREGROUND)
